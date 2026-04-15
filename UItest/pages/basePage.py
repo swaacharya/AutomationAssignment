2343be60c377d1c84914e18ec71a9399ea6e8dce
Acharya <swaacharya@deloitte.com>
2026-04-15 15:08:58 +0530

    Completed the dashboard validation and executed it

--- UItest/pages/basePage.py
+++ UItest/pages/basePage.py
@@ -5,19 +5,28 @@ class BasePage:
         self.page = page
         self.cookiesPopup = self.page.locator('[id="onetrust-close-btn-container"]')
 
+        #table headers locator
+        self.tableHeaders = self.page.locator('thead>tr>th>div>span')
+
     def navigateToURL(self, url):
         self.page.goto(url)
         self.page.bring_to_front()
     
-    def validatePageText(self, title):
+    def validatePageText(self, title, eleLoc=None):
         self.page.bring_to_front()
         self.page.wait_for_load_state("networkidle")
 
-        title = self.page.get_by_text(title, exact=True)
+        if eleLoc:
+            title = eleLoc.get_by_text(title, exact=True)
+        else:
+            title = self.page.get_by_text(title, exact=True)
         expect(title).to_be_visible()
 
     def closeCookiesPopup(self):
         self.cookiesPopup.click()
 
     def validateTableHeaders(self, tableHeaders):
-        pass
+        for i, header in enumerate(tableHeaders):
+            headerLocator = self.tableHeaders.nth(i)
+            expect(headerLocator).to_be_visible()
+            expect(headerLocator).to_have_text(header)
--- UItest/tests/test_002_Dashboard.py
+++ UItest/tests/test_002_Dashboard.py
@@ -3,3 +3,12 @@ from pages.dashboardPage import DashboardPage
 def test_dashboard_categories(page):
     dashboard = DashboardPage(page)
     dashboard.test_all_category()
+
+def test_parsuit_list_headers(page):
+    dashboard = DashboardPage(page)
+    headers = ["Client Name", "Initiatives | Pursuit ID", "Jupiter ID", "Country", "Requester", "Proposal Type", "Status", "Start Date", "End Date"]
+    dashboard.validateTableHeaders(headers)
+
+def test_menu_container_options(page):
+    dashboard = DashboardPage(page)
+    dashboard.validate_menu_container_options(["Collapse","Demand Pursuits", "Create Pursuit"])
